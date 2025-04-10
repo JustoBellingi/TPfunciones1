83709ad932e60c4d41ab266c7893c54c168528a8
JustoBellingi <jbellingi2006@gmail.com>
2025-04-10 12:14:07 -0300

    Create ejercicio7.py

---
+++ ejercicio7.py
@@ -0,0 +1,16 @@
+def operacion_basicas (a,b):
+    return suma, resta, multiplicacion, division
+
+a = float(input('ingresa un numero: ' ))
+b = float(input('ingresa un numero: ' ))
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+operacion_basicas (a,b)
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
