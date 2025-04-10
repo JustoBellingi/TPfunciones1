1303bffc394a9871562e329fe7ac5f067dab2bc3
JustoBellingi <jbellingi2006@gmail.com>
2025-04-10 10:00:05 -0300

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,1 +1,8 @@
+def segundos_a_horas (segundos):
+    return segundos / 3600
 
+segundos = (float(input("Ingresa la cantidad de segundos: ")))
+
+horas = segundos_a_horas(segundos)
+
+print(f"{segundos} segundos es igual a esta cantidad de {horas} horas.")
